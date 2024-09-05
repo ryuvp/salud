6fd788fe9f34799792d01dd1d6ae6002d4859e84
Emil Vasquez <vasquez_hco@hotmail.com>
2024-09-05 12:53:25 -0500

    05/09/2024 12:53

--- public/index.php
+++ public/index.php
@@ -31,7 +31,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php'; // Esta ruta debería ser correcta
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php'; // Esta ruta debería ser correcta
 
 $kernel = $app->make(Kernel::class);
 
