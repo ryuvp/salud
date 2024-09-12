a2e71412d2f023b2d801822c9138ccef268c8962
Emil <vasquez_hco@hotmail.com>
2024-09-11 19:57:37 -0500

    11/09/2024 19:57

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <link rel="icon" href="/favicon.ico">
+    <link rel="icon" href="/layout/images/logo-sm.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RENOPAC</title>
     <link id="theme-css" rel="stylesheet" type="text/css" href="/themes/aura-light-green/theme.css">
